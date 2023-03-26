e2256c61eb2106b2962b62023bca9db390ff2113
Maksim Shcherbo <max@happygopher.nl>
2023-03-26 21:27:27 +0100

    Implement Flexible Interactor Library for Running Use Cases (#2)
    * feat: Implement Interactor Library for UseCaseRunners
    
    Implement a flexible and efficient Interactor Library for managing
    use cases and dispatching requests based on request types. Inspired
    by Uncle Bob's Clean Architecture article, the library provides a
    Dispatcher, a UseCaseRunner interface, comprehensive error handling, and
    supports both pure functions and structures as use case implementations.
    The code is well-documented, thoroughly tested, and includes a detailed
    README with a working example.

---
+++ interactor_test.go
@@ -0,0 +1,42 @@
+package interactor_test
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/screwyprof/interactor"
+	. "github.com/screwyprof/interactor/testdsl"
+)
+
+func TestInteractorAcceptance(t *testing.T) {
+	t.Parallel()
+
+	t.Run("ensure use case returns an error", func(t *testing.T) {
+		t.Parallel()
+
+		want := errors.New("some error")
+		useCaseRunner := &ConcreteUseCase{err: want}
+		adaptedUseCaseRunner := interactor.MustAdapt(useCaseRunner.RunUseCase)
+
+		Test(t)(
+			Given(adaptedUseCaseRunner.Run),
+			When(context.Background(), TestRequest{}, &TestResponse{}),
+			ThenFailWith(want),
+		)
+	})
+
+	t.Run("ensure use case returns valid result", func(t *testing.T) {
+		t.Parallel()
+
+		want := &TestResponse{result: 123}
+		useCaseRunner := &ConcreteUseCase{}
+		adaptedUseCaseRunner := interactor.MustAdapt(useCaseRunner.RunUseCase)
+
+		Test(t)(
+			Given(adaptedUseCaseRunner.Run),
+			When(context.Background(), TestRequest{123}, &TestResponse{}),
+			Then(want),
+		)
+	})
+}
